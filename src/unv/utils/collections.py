a9c6bf773942a0c6e30923be349b36afff3a7b10
Morty Space <morty.space@gmail.com>
2019-01-03 00:32:01 +0200

    Added simple doctest

--- src/unv/utils/collections.py
+++ src/unv/utils/collections.py
@@ -1,11 +1,23 @@
-import collections.abc as abc
+def update_nested_dict(original: dict, from_dict: dict) -> dict:
+    """Simple function to update nested dict.
 
-
-def update_nested_mapping(mapping, update_mapping):
-    """Simple function to update nested dict."""
-    for key, value in update_mapping.items():
-        if isinstance(value, abc.Mapping) and key in mapping:
-            mapping[key] = update_nested_mapping(mapping.get(key, {}), value)
+    >>> d = {0: 0, 1: {1: 1}, 2: {2: {3: 3}}}
+    >>> n = {0: 1, 2: {2: {4: 4}}, 5: 1}
+    >>> r = update_nested_dict(d, n)
+    >>> r is d
+    True
+    >>> r[0]
+    1
+    >>> r[1]
+    {1: 1}
+    >>> r[2][2]
+    {3: 3, 4: 4}
+    >>> r[5]
+    1
+    """
+    for key, value in from_dict.items():
+        if isinstance(value, dict) and key in original:
+            original[key] = update_nested_dict(original.get(key, {}), value)
         else:
-            mapping[key] = value
-    return mapping
+            original[key] = value
+    return original
